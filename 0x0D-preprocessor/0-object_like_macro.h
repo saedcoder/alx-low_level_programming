c75924f1b66f441affdd4c7517879e890cccb5e5
saed <saed.madkaash@gmail.com>
2023-08-14 03:29:18 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,13 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
 #define SIZE 1024
+
 #endif

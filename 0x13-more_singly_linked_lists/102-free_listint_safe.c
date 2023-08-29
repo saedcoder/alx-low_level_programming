e513c0caf0739a31bd4e5b4685d3c0231dab6789
saed <saed.madkaash@gmail.com>
2023-08-29 00:25:40 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,7 +5,7 @@ size_t free_listint_safe(listint_t **h);
 
 /**
  * looped_listint_count - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
+ *                        in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
@@ -52,10 +52,9 @@ size_t looped_listint_count(listint_t *head)
 }
 
 /**
- * free_listint_safe - Frees a listint_t list safely (ie.
- *                     can free lists containing loops)
- * @h: A pointer to the address of
- *     the head of the listint_t list.
+ * free_listint_safe - Frees a listint_t list safely (i.e.,
+ *                     can free lists containing loops).
+ * @h: A pointer to the address of the head of the listint_t list.
  *
  * Return: The size of the list that was freed.
  *
@@ -77,7 +76,6 @@ size_t free_listint_safe(listint_t **h)
 			*h = tmp;
 		}
 	}
-
 	else
 	{
 		for (index = 0; index < nodes; index++)

3c77d3d434ba4ba97bd5b2e74a98f6120421c844
saed <saed.madkaash@gmail.com>
2023-08-31 04:22:53 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
- * get_bit - Gets the value of a bit at a given index.
- * @n: The number to get the bit from.
- * @index: The index of the bit to retrieve.
- *
- * Return: The value of the bit at the given index, or -1 if an error occurred.
+ * get_bit - return one bit
+ * @n: the number
+ * @index: the position to read the bit
+ * Description: return 1 bit
+ * section header: the header of this function is holberton.h
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int bit;
 
-	max <<= index;
-
-	if (max == 0)
+	if (index > 64)
 		return (-1);
 
-	if ((n & max))
-		return (1);
-	else
-		return (0);
+	bit = n >> index;
+
+	return (bit & 0x1);
 }
